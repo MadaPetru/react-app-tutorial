c23754eaff6262d8887da435c7c565e5a3872a77
mada.petru <mada.petru@digitalexplorer.ro>
2024-02-24 22:57:38 +0200

    start tutorial, need to add more features then learning unit tests and better understanding for the last part of historisation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,165 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class Board extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+    renderSquare(index) {
+        return (
+            <Square value={this.props.squares[index]}
+                    onClick={() => {
+                        this.props.onClick(index)
+                    }}
+            />
+        );
+    }
+}
+
+function Square(props) {
+    return (
+        <button className="square"
+                onClick={() => {
+                    props.onClick()
+                }}>
+            {props.value}
+        </button>
+    );
+}
+
+class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array.of(9).fill(null)
+            }],
+            xIsNext: true,
+            isMatchFinished: false,
+            stepNumber: 0
+        }
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        let nextValueOfThePlayer = this.getNextValueOfThePlayer();
+        let status = 'Next player: ' + nextValueOfThePlayer;
+        if (winner) {
+            this.state.isMatchFinished = true;
+            status = "Winner: " + nextValueOfThePlayer;
+        }
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move # ' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(index) => this.handleClick(index)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>
+                        {status}
+                    </div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    };
+
+    handleClick(index) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        if (this.state.isMatchFinished) return;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (squares[index] != null) return;
+        squares[index] = this.getNextValueOfThePlayer();
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length
+        });
+    }
+
+    jumpTo(move) {
+        this.setState({
+            stepNumber: move,
+            xIsNext: (move % 2) === 0
+        })
+    }
+
+    getNextValueOfThePlayer() {
+        return this.state.xIsNext ? 'X' : 'O';
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Game/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
